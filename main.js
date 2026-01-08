2385d8f0b16c0ec6edff8c79b9d5e171d357be2e
Kevin Martins <k.martins@newrest-wagonslits.eu>
2026-01-08 12:20:05 +0100

    feat: add date value even when empty

--- main.js
+++ main.js
@@ -1,21 +1,42 @@
 const dateToSeconds = (date) => {
     const res = date.split(/[a-z]+/g);
     res.pop()
+    if (res[0] === "0") return 0;
     switch (res.length) {
         case 1: return parseInt(res[0]);
         case 2: return parseInt(res[0]) * 60 + parseInt(res[1]);
         case 3: return parseInt(res[0] * 3600) + parseInt(res[1] * 60) + parseInt(res[2]);
+        default: return 0;
     };
 };
 
 const textArea = document.getElementById('textarea');
 const stateText = document.getElementById('stateText');
+let res = [];
 
-const loadingText = document.getElementById('loadingText');
+// const generateTable = (csvString) => {
+//     const tableBody = document.getElementById('table-body');
+//     tableBody.innerHTML = ''; // Clear existing rows
+//     const lines = csvString.split('\n');
+//     console.log(lines);
+//     for (let line of lines) {
+//         if (line.trim() === '') continue;
+//         const cells = line.split(';');
+//         console.log(cells);
+//         const row = document.createElement('tr');
+//         const cell = document.createElement('td');
+//         for (let cellValue of cells) {
+//             cell.innerHTML = cellValue;
+//             console.log(cellValues);
+//             row.appendChild(cell);
+//         }
+//         tableBody.appendChild(row);
+//     }
+// }
 
 const organiseData = () => {
     const text = textArea.value.replace(/(<([^>]+)>)/ig, '\n').split('\n');
-    const res = [];
+    res = [];
     const length = text.length;
     for (let i = 0; i < length; i++) {
         if (text[i].length > 0) {
@@ -45,6 +66,7 @@ const organiseData = () => {
     if (res[2] !== "Temps en secondes") {
         res.splice(2, 0, "Temps en secondes");
     }
+    console.log(res);
     return res;
 }
 
@@ -62,6 +84,8 @@ const formatCSV = (data) => {
 
 function copyFile() {
     const data = organiseData();
-    navigator.clipboard.writeText(formatCSV(data));
+    const csvContent = formatCSV(data);
+    // generateTable(csvContent);
+    navigator.clipboard.writeText(csvContent);
     alert("text copied");
 };
